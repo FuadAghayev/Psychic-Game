9ad2cf05d5678a089b691546a8d1ced761b6ba46
Fuad Aghayev <39545726+FuadAghayev@users.noreply.github.com>
2018-08-25 23:48:15 -0500

    Add files via upload

---
+++ Psychic-Game/assets/javascript/game.js
@@ -0,0 +1,11 @@
+document.write()
+ // Here we create our random numbers
+
+ var confirmwin= confrim(win);
+
+ var confirmlose= confirm(lose)
+
+ var letguessesleft=confirm(guesses)
+
+ //Setting all to zero
+ var rand_letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
